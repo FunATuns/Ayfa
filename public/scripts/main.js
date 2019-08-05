45edc8e407889b8bf1fc0a66237f70a4b717f4f6
FunATuns <payton.obrycki@gmail.com>
2019-08-05 17:34:37 -0500

    Clean Up Part 1
    lololololololol lets go boys, got rid of a bunch of shit we didn't need

---
+++ public/scripts/main.js
@@ -0,0 +1,12 @@
+// Connection url
+var socket = io.connect("http://141.126.155.58:7777");
+
+/*
+  // Example of Socket Emit
+  socket.emit('Joined',null);
+
+  // Example of Socket On
+  socket.on('Caught', function(data){
+    alert("You/your runner got CAUGHT!");
+  });
+*/
