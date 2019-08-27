1e0b08db88e09e16ca62830b8ad2538d01b5e95a
Noise859 <brennan.neuser@gmail.com>
2019-08-26 21:43:56 -0500

    Stuff n things

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -10,3 +10,27 @@ var socket = io.connect("http://141.126.155.58:7777");
     alert("You/your runner got CAUGHT!");
   });
 */
+
+
+function startGame() {
+  if($("#user").val() != "" && $("#user").val() != null) {
+    socket.emit("Login", $("#user").val());
+  }
+  else {
+    $("#noname").css("display", "block");
+  }
+}
+
+socket.on("StartGame", function(myPlayer){
+  localStorage.setItem("myCID", myPlayer.cid);
+  localStorage.setItem("myExploring", myPlayer.exploring);
+  localStorage.setItem("myFood", myPlayer.food);
+  localStorage.setItem("myID", myPlayer.id);
+  localStorage.setItem("myStrength", myPlayer.strength);
+  localStorage.setItem("myUsername", myPlayer.username);
+  window.location = "game.html";
+});
+
+function starting() {
+  console.log(localStorage.getItem("myUsername"));
+}
